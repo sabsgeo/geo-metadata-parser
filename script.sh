e2aaca9947d3746091bf86635ea1a18b509c9acf
Sabu George <sabugeorge.mec@gmail.com>
2023-06-03 18:06:47 +0530

    adding sample level metadata

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
-python main.py --function add_series_metadata --process 10
-python main.py --function sync_metadata_status_from_geo --process 10
+python main.py --function add_series_metadata --process 10 --min_memory 100
+python main.py --function sync_metadata_status_from_geo --process 10 --min_memory 100
+python main.py --function add_sample_metadata --process 10 --min_memory 100
