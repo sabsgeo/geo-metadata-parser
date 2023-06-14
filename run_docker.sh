9d64ad3e421c45d0bac57605920d4354782bdf60
Sabu George <sabugeorge.mec@gmail.com>
2023-06-14 05:58:49 +0530

    Storing only unique samples

---
+++ run_docker.sh
@@ -0,0 +1,3 @@
+sudo docker run -d geo-metadata-parser --function sync_status_from_geo --number_of_process 50 --min_memory 100
+sudo docker run -d geo-metadata-parser --function add_update_metadata --number_of_process 50 --min_memory 100 --shuffle
+sudo docker run -d geo-metadata-parser --function validate_sample
