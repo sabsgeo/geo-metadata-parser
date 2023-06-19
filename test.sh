3978cda73371b5097db143d89166e15d911d0359
Sabu George <sabugeorge.mec@gmail.com>
2023-06-19 10:02:18 +0530

    adding test

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nohup python test_0.py & 
+nohup python test_1.py & 
+nohup python test_2.py & 
+nohup python test_3.py & 
+nohup python test_4.py & 
+nohup python test_5.py & 
+nohup python test_6.py & 
+nohup python test_7.py & 
+nohup python test_8.py & 
+nohup python test_9.py & 
