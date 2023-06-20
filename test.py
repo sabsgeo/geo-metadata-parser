d629edc0e1708a1d4ebff301c6f9922398fc3e35
Sabu George <sabugeorge.mec@gmail.com>
2023-06-20 21:17:41 +0530

    Making the setup locally

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 from geo import geo_mongo
 
+import json
+
+from pymongo import InsertOne
+
 inst = geo_mongo.GeoMongo()
+
+with open("pubmed_info_final.json") as f:
+    json_data = json.load(f)
+    opera = []
+    for k in json_data:
+        data_write = json_data[k].copy()
+        data_write["_id"] = data_write.get("pmid")
+        opera.append(InsertOne(data_write))
+        print(data_write)
+    inst.pubmed_metadata_collection.bulk_write(opera, ordered=False)
