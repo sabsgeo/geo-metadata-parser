a2385f9ccc5c80eeb73920447078de69d22ebd22
Sabu George <sabugeorge.mec@gmail.com>
2023-07-02 10:27:57 +0530

    making rand give always alpha numeric

---
+++ watch_dog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ST=$(sudo systemctl status mongod | grep "Active" | grep "running")
+DD=$(date)
+if [ -z "$ST" ]; then
+        echo "${DD}:- Mongo DB need to be restarted"
+        /home/ubuntu/geo-metadata-parser/stop_docker.sh
+        sudo systemctl restart mongod
+        sleep 5
+        /home/ubuntu/geo-metadata-parser/run_docker.sh
+else
+        echo "${DD}:- Everything is good"
+fi
