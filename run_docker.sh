f09b9cccdaa8959c4eb2d38703590c3db99e503a
Sabu George <sabugeorge.mec@gmail.com>
2023-07-02 08:13:02 +0530

    making rand give always alpha numeric

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 git pull
 sudo docker build -t geo-metadata-parser .
-RAND=$(openssl rand -base64 6)
+RAND=$(openssl rand -hex 6)
 sudo docker run --net=host --name=sync_status_from_geo_$RAND -d geo-metadata-parser --function sync_status_from_geo --for_n_days 20 --number_of_process 10 --min_memory 100
 sudo docker run --net=host --name=add_update_metadata_$RAND -d geo-metadata-parser --function add_update_metadata --number_of_process 20 --min_memory 100 --shuffle
 sudo docker run --net=host --name=validate_sample_$RAND -d geo-metadata-parser --function validate_sample --run_interval 2880
